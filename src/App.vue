131add98205506ead0ff0886bdd4cc9f89e8d3ec
Maurocazzato <maurizzio.125@gmail.com>
2023-12-20 14:13:50 +0100

    h1 inserito

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AppTitle from './components/AppTitle.vue'
 </script>
 
 <template>
@@ -11,7 +11,8 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppTitle />
+  <h1>La mia prima app con Vite!</h1>
 </template>
 
 <style scoped>
@@ -21,10 +22,11 @@ import HelloWorld from './components/HelloWorld.vue'
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+}</style>
